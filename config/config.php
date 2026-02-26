4d8f6229b14b3422fcb6cf3d8b7229ee5388ab1b
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2026-02-26 10:56:04 -0300

    incrementa arquivo .env

--- config/config.php
+++ config/config.php
@@ -1,2 +1,6 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 date_default_timezone_set('America/Sao_Paulo');
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
