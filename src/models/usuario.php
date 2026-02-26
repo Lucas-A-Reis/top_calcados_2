a21f95b0acb3409734ea627f64266890bcc94cd8
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2026-02-26 15:08:40 -0300

    cria classe Usuario

---
+++ src/models/usuario.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+class Usuario {
+    private ?int $id = null;
+    private string $nome;
+    private string $email;
+    private string $senha;
+    private ?string $telefone = null;
+
+    // --- GETTERS ---
+    public function getId(): ?int { return $this->id; }
+    public function getNome(): string { return $this->nome; }
+    public function getEmail(): string { return $this->email; }
+    public function getSenha(): string { return $this->senha; }
+    public function getTelefone(): ?string { return $this->telefone; }
+
+    // --- SETTERS ---
+    public function setId(int $id): void { $this->id = $id; }
+    public function setNome(string $nome): void { $this->nome = $nome; }
+    public function setEmail(string $email): void { $this->email = $email; }
+    public function setSenha(string $senha): void { $this->senha = $senha; }
+    public function setTelefone(?string $telefone): void { $this->telefone = $telefone; }
+
+}
