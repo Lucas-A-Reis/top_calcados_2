e155a19d0a74d7400119891ff3d9a75b241272cb
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2026-02-26 16:42:47 -0300

    acrescenta validação de dados à função sanitizar

--- src/helpers/funcoes_uteis.php
+++ src/helpers/funcoes_uteis.php
@@ -1,21 +1,37 @@
 <?php
-function sanitizar($valor, $tipo)
+function sanitizar($valor, $tipo, $substrato_do_numero = null)
 {
+
+    $valor = trim($valor);
+
     switch ($tipo) {
+
         case 'string':
             return filter_var($valor, FILTER_SANITIZE_SPECIAL_CHARS);
 
         case 'email':
-            return filter_var($valor, FILTER_SANITIZE_EMAIL);
+            $email_limpo = filter_var($valor, FILTER_SANITIZE_EMAIL);
+            if (!filter_var($email_limpo, FILTER_VALIDATE_EMAIL)) {
+                throw new AppException('E-mail inválido', 'warning');
+            }
+            return $email_limpo;
 
         case 'telefone':
             return preg_replace('/[^0-9]/', '', $valor);
 
         case 'float':
-            return filter_var($valor, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+            $float_limpo =  filter_var($valor, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+            if (!filter_var($float_limpo, FILTER_VALIDATE_FLOAT)) {
+                throw new AppException('O preço informado não é um número decimal válido', 'warning');
+            }
+            return (float)$float_limpo;
 
         case 'int':
-            return filter_var($valor, FILTER_SANITIZE_NUMBER_INT);
+            $int_limpo = filter_var($valor, FILTER_SANITIZE_NUMBER_INT);
+            if (!filter_var($int_limpo, FILTER_VALIDATE_INT) && $valor !== "0"){
+                throw new AppException( $substrato_do_numero.' informado não é um número decimal válido', 'warning');
+            }
+            return (int)$int_limpo;
 
         case 'none':
             return $valor;
